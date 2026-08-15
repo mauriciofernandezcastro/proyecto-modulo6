e5e46f023f58ea9db189675da9f0a7b30940b9ab
Mauricio Fernandez <77501857+mauriciofernandezcastro@users.noreply.github.com>
2026-08-15 16:38:11 -0400

    Remove comments and clean up mainController.js

--- proyecto-modulo6/controllers/mainController.js
+++ proyecto-modulo6/controllers/mainController.js
@@ -1,11 +1,7 @@
-// Controladores simples, sin cosas raras
-
-// Ruta / devuelve HTML
 function home(req, res) {
   res.send('<h1>Bienvenido a mi App - Modulo 6</h1><p>Esta es la ruta principal / en HTML</p><p><a href="/status">Ver status en JSON</a></p><p><a href="/info.txt">Ver archivo estatico</a></p>');
 }
 
-// Ruta /status devuelve JSON
 function status(req, res) {
   res.json({
     status: 'ok',
